c44ecab578d06df0f3025e37b53137ede75efe89
黄水生 <2274751790@qq.com>
2022-05-10 12:46:44 +0800

    fix: 修改env配置

--- config/utils/outputStaticUrl.ts
+++ config/utils/outputStaticUrl.ts
@@ -1,5 +1,5 @@
-export const APP_NAME = process.env.REACT_APP_RELEASE_PROJECT_NAME;
-export const APP_ENV = process.env.REACT_APP_RELEASE_PROJECT_ENV;
+export const APP_NAME = process.env.VUE_APP_RELEASE_PROJECT_NAME;
+export const APP_ENV = process.env.VUE_APP_RELEASE_PROJECT_ENV;
 export const NODE_ENV = process.env.NODE_ENV;
 
 export const outputStaticUrl = (isProduction: boolean) => {
