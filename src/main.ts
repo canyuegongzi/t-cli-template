24521ffddf9ae61a0f297234afdeeafa196c675f
shuisheng <2274751790@qq.com>
2022-07-04 09:22:35 +0800

    feat: 测试

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
-import { createApp } from 'vue';
+// import { createApp } from 'vue';
 
-import App from './App.vue';
+// import App from './App.vue';
 
-import router from '@/router/index';
-import store from '@/store/index';
-import '@/assets/css/index.scss';
+// import router from '@/router/index';
+// import store from '@/store/index';
 
-const app = createApp(App);
+// console.log(process.env);
+// console.table(process.env);
 
-app.use(store);
-app.use(router);
+// const app = createApp(App);
 
-app.mount('#app');
+// app.use(store);
+// app.use(router);
+
+// app.mount('#app');
+
+const aaa = 221241;
+
+const eat = () => {
+  console.log('eat');
+};
+console.log(aaa);
+console.log(eat);
+
+const dd = [2, 4, 6];
+const gg = dd.every((v) => v + 1);
+console.log(gg);
+
+// Promise是新增的语法，浏览器支持就支持，不支持就只能polyfill。即挂载一个全局的Promise
+const c = new Promise((res, rej) => {});
+
+// async是es8的语法，其实本质上是es6生成器的语法糖，会转换成生成器语法
+const ddd = async () => {};
+console.log(ddd, 3333);
