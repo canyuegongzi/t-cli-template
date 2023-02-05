ecdf131fe520db5508cab1e2fa1882b9031df3f2
fei <yongfeide123@sina.com>
2023-02-05 16:50:09 +0800

    add config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [ '@typescript-eslint/eslint-plugin' ],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
@@ -16,9 +16,22 @@ module.exports = {
     jest: true,
   },
   rules: {
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'prefer-rest-params': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    'semi':[ 'error','always' ],
+    'prefer-spread': 'off',
+    "array-bracket-spacing": [ "error","always" ],
+    "object-curly-spacing": [ "error","always" ]
   },
 };
